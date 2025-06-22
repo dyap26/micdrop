371ac9f9022b2d4578952a1226d6eb9f1da75482
dyap26 <dyap@terpmail.umd.edu>
2025-06-22 19:29:01 -0400

    Fixed bugs and structure

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,33 +1,40 @@
-// AppNavigator.tsx or AppNavigator.jsx (TypeScript assumed)
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { RootStackParamList } from '../types/navigation';
 import React from 'react';
 
 import AlbumDetailScreen from '../screens/AlbumDetailScreen';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
 
 type AppNavigatorProps = {
   isLoggedIn: boolean;
 };
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator({ isLoggedIn }: AppNavigatorProps) {
   return (
     <NavigationContainer>
-      <Stack.Navigator id={undefined}>
-        {isLoggedIn ? (
-          <>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="AlbumDetail" component={AlbumDetailScreen} />
-          </>
-        ) : (
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
+      <Stack.Navigator id={undefined} initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+
+        {/* Auth Screens */}
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{ headerShown: false }}
+        />
+
+        {/* Protected screen */}
+        {isLoggedIn && (
+          <Stack.Screen name="AlbumDetail" component={AlbumDetailScreen} />
         )}
       </Stack.Navigator>
     </NavigationContainer>
